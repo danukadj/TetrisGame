1b8b5ce994b4bb87bbcbb95eef6346663a4cb6a4
danukadj <danukasandaruwandj@gmail.com>
2020-12-21 16:21:24 +0530

    movedown fun

--- app.js
+++ app.js
@@ -58,7 +58,20 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[currentPosition+index].classList.remove('tetromino')
         })
     }
+    //make the tetromino move down every second
+    timerId = setInterval(moveDown,1000)
 
+    //move down function
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+    }
+
+
+
+
+    
 
 
 
