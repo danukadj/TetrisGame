4a1e795db4a1004a7c75d8ee923fa542e4808436
danukadj <danukasandaruwandj@gmail.com>
2020-12-21 17:24:22 +0530

    moveleft function and keylistner

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ document.addEventListener('DOMContentLoaded', () => {
     //make the tetromino move down every second
     timerId = setInterval(moveDown,1000)
 
+    //assign function to keycodes
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            //rotate
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup',control)
+
     //move down function
     function moveDown() {
         undraw()
@@ -81,9 +95,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-
-
-
+    //move the tetromino left, unless is at the edge or there is a blockage
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+        if(!isAtLeftEdge) currentPosition -= 1
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+        draw()
+    }
 
 
 
