b43d109e6d0dc53bbe0458b87852ced030ddd36c
danukadj <danukasandaruwandj@gmail.com>
2020-12-21 16:14:49 +0530

    draw undraw fun

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
         [width,width*2,width*2+1,width*2+2]
     ]
     const zTetromino = [
-        [width*2,width+1,width*2+1,width+2],
         [0,width,width+1,width*2+1],
         [width*2,width+1,width*2+1,width+2],
-        [0,width,width+1,width*2+1]
+        [0,width,width+1,width*2+1],
+        [width*2,width+1,width*2+1,width+2]
+        
     ]
     const tTetromino = [
         [width,1,width+1,width+2],
@@ -36,7 +37,27 @@ document.addEventListener('DOMContentLoaded', () => {
         [1,width+1,width*2+1,width*3+1],
         [width,width+1,width+2,width+3]
     ]
+    const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
 
+    let currentPosition = 4
+    let currentRotation = 0
+
+    //randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length)
+    let current = theTetrominoes[random][currentRotation]
+
+    //draw the tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition+index].classList.add('tetromino')
+        })
+    }
+    //undraw the tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition+index].classList.remove('tetromino')
+        })
+    }
 
 
 
