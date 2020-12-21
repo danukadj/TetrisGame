d7af1b8330230958963a27ab425f959a9c39f3f2
danukadj <danukasandaruwandj@gmail.com>
2020-12-21 15:50:05 +0530

    tetrominoes

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+    //the tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [width*2,1,width+1,width*2+1],
+        [width,width*2,width*2+1,width*2+2]
+    ]
+    const zTetromino = [
+        [width*2,width+1,width*2+1,width+2],
+        [0,width,width+1,width*2+1],
+        [width*2,width+1,width*2+1,width+2],
+        [0,width,width+1,width*2+1]
+    ]
+    const tTetromino = [
+        [width,1,width+1,width+2],
+        [1,width+1,width*2+1,width+2],
+        [width,width+1,width*2+1,width+2],
+        [width,1,width+1,width*2+1]
+    ]
+    const oTetromino = [
+        [0,width,1,width+1],
+        [0,width,1,width+1],
+        [0,width,1,width+1],
+        [0,width,1,width+1]
+    ]
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+
+
 
 
     
